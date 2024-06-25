fb7c658b12c1e7ef43fb7998a564002152d47cee
Ram-Jam5 <edgeillusive@gmail.com>
2024-06-25 10:51:11 -0500

    create sign-in functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,10 +9,6 @@ router.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs");
 });
 
-router.get("/sign-in", (req, res) => {
-    res.render("auth/sign-in.ejs");
-  });
-  
 
 router.post("/sign-up", async (req, res) => {
     const userInDatabase = await User.findOne({ username: req.body.username });
@@ -22,12 +18,35 @@ router.post("/sign-up", async (req, res) => {
     if (req.body.password !== req.body.confirmPassword) {
         return res.send("Password and Confirm Password must match");
     }
-
+    
     const hashedPassword = bcrypt.hashSync(req.body.password, 10); 
     req.body.password = hashedPassword;
-
+    
     const user = await User.create(req.body);
     res.send(`Thanks for signing up ${user.username}`);
+});
+
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs");
+  });
+  
+router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send("Login failed. Please try again.")
+    }
+
+    const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password);
+
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+    };
+    req.session.user = {
+        username: userInDatabase.username,
+    }
+    res.redirect("/");
+
+    
 });
 
 module.exports = router;
